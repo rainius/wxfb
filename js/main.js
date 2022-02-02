0bfe2caaef7b9e05b79efeb22d1d18d01a5e4b53
Zhang Jing <jing@ZhangdeMacBook-Pro-2.local>
2022-02-02 22:57:26 +0800

    Show a temp bg

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+
+
 const ctx = canvas.getContext('2d')
 
 
+const ctx = canvas.getContext('2d')
+
+/**
+ * 游戏主函数
+ */
+export default class Main {
+  constructor() {
+    let image = new Image();
+    image.src = '../images/background.png';
+    image.onload = ()=>{
+      this.ctx.drawImage(image,
+        0, 0, 
+        image.width, image.height,
+        0, 0,
+        image.width, image.height);
+    }
+  }
+
+}
