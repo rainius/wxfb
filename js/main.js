26e9b17d7f71fd3c53939a1f3692e9bd5503c50a
Zhang Jing <jing@ZhangdeMacBook-Pro-2.local>
2022-02-03 07:12:57 +0800

    ctx is not a member of Main

--- js/main.js
+++ js/main.js
@@ -2,18 +2,15 @@
 
 const ctx = canvas.getContext('2d')
 
-
-const ctx = canvas.getContext('2d')
-
 /**
  * 游戏主函数
  */
 export default class Main {
   constructor() {
     let image = new Image();
-    image.src = '../images/background.png';
+    image.src = 'images/bg.jpg';
     image.onload = ()=>{
-      this.ctx.drawImage(image,
+      ctx.drawImage(image,
         0, 0, 
         image.width, image.height,
         0, 0,
