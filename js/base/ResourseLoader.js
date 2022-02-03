66a6d830439e8824cc4cff614ac651701570cdf4
Zhang Jing <jingz.bit@gmail.com>
2022-02-03 12:19:16 +0800

    add onLoad() for loader

--- js/base/ResourseLoader.js
+++ js/base/ResourseLoader.js
@@ -1,5 +1,4 @@
 //资源加载类，确保canvas在图片资源加载完成后才进行渲染
-
 import { Resources } from "./Resource";
 
 export class ResourceLoader {
@@ -13,5 +12,24 @@ export class ResourceLoader {
             this.map.set(key, image);
         }
         console.log(this.map);
+    }
+
+    //注入回调函数
+    onload(callback) {
+        // 计数
+        let loadCount = 0;
+        for (let value of this.map.values()) {      //获取所有的图片对象
+            value.onload = () => {                  //图片加载函数
+                loadCount++;                        //加载计数
+                if (loadCount >= this.map.size) {
+                    callback(this.map);             //资源已经全部加载，则调用callback
+                }
+            }
+        }
+    }
+
+    //工厂方法
+    static create() {
+
     }
 }
