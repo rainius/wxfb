2b6e6ffd67a84eb82f611cee1843e4dba7359c08
Zhang Jing <jingz.bit@gmail.com>
2022-02-04 07:33:58 +0800

    地面循环无缝绘制

--- js/player/Land.js
+++ js/player/Land.js
@@ -13,14 +13,17 @@ export class Land extends Sprite {
             image.width, image.height);
 
             this.landX = 0;
-            this.landSpeed = Director.getInstance().moveSpeed;
+            this.landSpeed = -Director.getInstance().moveSpeed;
     }
 
     // 重写draw()以实现地面不停左移
     draw() {
         this.landX = this.landX + this.landSpeed;
+        if (this.landX < DataStore.getInstance().canvas.width - this.img.width) {
+            this.landX = 0;
+        }
         super.draw(this.img, 
             this.srcX, this.srcY, this.srcW, this.srcH,
-            -this.landX, this.y, this.width, this.height);
+            this.landX, this.y, this.width, this.height);
     }
 }
