9e90aedc24bd44a44e130247eae22762967383a1
Zhang Jing <jingz.bit@gmail.com>
2022-02-04 00:14:41 +0800

    建立Land类并初始化

---
+++ js/player/Land.js
@@ -0,0 +1,14 @@
+//不断移动的地面
+
+import { DataStore } from "../base/DataStore";
+import { Sprite } from "../base/Sprite";
+
+export class Land extends Sprite {
+    constructor() {
+        const image = Sprite.getImage('land');
+        super(this.image, 
+            0, 0, image.width, image.height,
+            0, DataStore.getInstance().canvas.height - image.height,
+            image.width, image.height);
+    }
+}
