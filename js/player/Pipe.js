00c52c754222ff30a174ea6ea03cb6f6dc9e74f4
Zhang Jing <jingz.bit@gmail.com>
2022-02-04 08:18:29 +0800

    Pipe:重写draw()

--- js/player/Pipe.js
+++ js/player/Pipe.js
@@ -8,8 +8,15 @@ export class Pipe extends Sprite {
             0, 0, image.width, image.height, 
             canvas.width, 0,     // 从屏幕右侧边缘外产生
             image.width, image.height);
-
+            //描述水管高度的属性
             this.top = top;
-            this.moveSpeed = -Director.getInstance().moveSpeed;
+            //运动速度
+            this.moveSpeed = -Director.getInstance().moveSpeed; 
+    }
+
+    //重写
+    draw() {
+        this.x = this.x + this.moveSpeed;   //随帧移动
+        super.draw(this.img, 0, 0, this.width, this.height, this.x, this.y, this.width, this.height);
     }
 }
