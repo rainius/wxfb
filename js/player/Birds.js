85b151787e817ebfb14a9c4b394a212b85b1e076
Zhang Jing <jingz.bit@gmail.com>
2022-02-05 08:36:24 +0800

    修改一些问题

--- js/player/Birds.js
+++ js/player/Birds.js
@@ -17,16 +17,18 @@ export class Birds extends Sprite {
             //存储小鸟状态
             const birdWidth = 34;   //小鸟宽度
             const birdHeight = 24;  //小鸟高度
+
             //小鸟在屏幕上的位置
             this.birdX = this.dataStore.canvas.width / 4;
             this.birdY = this.dataStore.canvas.height / 2;
+            this.birdWidth = birdWidth;
+            this.birdHeight = birdHeight;
 
             this.clippingX = [0, birdWidth , birdWidth * 2]   //3帧在图片中起始位置
             this.clippingY = [0, 0, 0];
             this.clippingWidth = [birdWidth, birdWidth, birdWidth];
             this.clippingHeight = [birdHeight, birdHeight, birdHeight];
             
-
             //计数：显示哪一帧
             this.count = 0;
             //帧索引
@@ -34,17 +36,16 @@ export class Birds extends Sprite {
     }
 
     draw() {
+
         //实现小鸟振翅动画
-        //定义帧间隔
+        //定义动画帧切换间隔
         const interval = 5;
-        
-        // this.index = this.count;
+        //按帧绘制小鸟
         super.draw(this.img,
             this.clippingX[this.index], this.clippingY[this.index],
             this.clippingWidth[this.index], this.clippingHeight[this.index],
             this.birdX, this.birdY,
-            34,24);
-            //this.birdWidth, this.birdHeight);
+            this.birdWidth, this.birdHeight);
 
         this.count = this.count + 1;
         if (this.count % interval == 0) {
