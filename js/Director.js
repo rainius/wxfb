ae0baa3aa722dce248a2325850d81b4c0b6e723e
Zhang Jing <jingz.bit@gmail.com>
2022-04-21 09:22:26 +0800

    play audio effects

--- js/Director.js
+++ js/Director.js
@@ -2,6 +2,7 @@
 import { DataStore } from "./base/DataStore";
 import { UpPipe } from "./runtime/UpPipe";
 import { DownPipe } from "./runtime/DownPipe";
+import Music from "./base/Music";
 export class Director {
 
     //导演只有一个，用单例实现
@@ -17,7 +18,7 @@ export class Director {
         // 持有数据存储对象
         this.dataStore = DataStore.getInstance();
         // 定义场景移动速度：像素/帧
-        this.moveSpeed = 2; 
+        this.moveSpeed = 2;
         // 游戏结束标记
         this.isGameOver = false;
         // 进入计分区标记
@@ -26,8 +27,8 @@ export class Director {
         this.outScoreArea = true;
     }
 
-     //产生管道对
-     createPipes() {
+    //产生管道对
+    createPipes() {
         // 管道显示范围约束
         const minTop = this.dataStore.canvas.height / 8;
         const maxTop = this.dataStore.canvas.height / 2;
@@ -39,8 +40,10 @@ export class Director {
     }
 
     //定义触屏事件的响应函数
-    onTouch() { 
+    onTouch() {
         //小鸟进行一次振翅飞行
+        //播放音效
+        Music.getInstance().playWing();
         this.dataStore.get('birds').flyUp();
     }
 
@@ -48,16 +51,19 @@ export class Director {
     check() {
         const birds = this.dataStore.get('birds');
         const land = this.dataStore.get('land');
-        console.log("检测 撞击地面： ", birds.birdY, birds.birdHeight,land.y);
+        console.log("检测 撞击地面： ", birds.birdY, birds.birdHeight, land.y);
         //检测与地面的碰撞
         if (birds.birdY + birds.birdHeight >= land.y) {
             console.log("撞击地面");
             this.isGameOver = true;
+            //播放音效
+            Music.getInstance().playHit();
+            return;
         }
         //检测与水管的碰撞
         //小鸟边框模型
         const birdsBorder = {
-            top: birds.y,  
+            top: birds.y,
             bottom: birds.birdY + birds.birdHeight,
             left: birds.birdX,
             right: birds.birdX + birds.birdWidth
@@ -73,10 +79,12 @@ export class Director {
                 bottom: pipe.y + pipe.height,
                 left: pipe.x,
                 right: pipe.x + pipe.width
-            }    
+            }
             //检查当前水管与小鸟是否碰撞
             if (Director.isCollisionDetected(birdsBorder, pipeBorder)) {
                 console.log("小鸟撞水管了");
+                //播放音效
+                Music.getInstance().playHit();
                 this.isGameOver = true;
                 break;
             }
@@ -86,25 +94,27 @@ export class Director {
         for (let j = 0; j < length; j += 2) {
             const pipeTop = pipes[j];
             //得分区边界：已经过碰撞检测，只考虑x方向边界
-            const scoreAreaBorder = {   
+            const scoreAreaBorder = {
                 left: pipeTop.x,
                 right: pipeTop.x + pipeTop.width
             }
             const score = this.dataStore.get('score');  //取计分对象
-             if (!Director.isInScoreArea(birdsBorder, scoreAreaBorder)) {//不在得分区
-                 this.outScoreArea = true;  
-                 if (this.inScoreArea) { 
-                     //如果前一状态仍然表示停留在得分区，则表明：
-                     //小鸟刚刚安全通过得分区，应当加分
-                     this.inScoreArea = false;
-                     score.increase();
-                     break;
-                 }
-             } else {   // 当前进入得分区
-                 this.inScoreArea = true;
-                 this.outScoreArea = false;
-                 break;
-             }
+            if (!Director.isInScoreArea(birdsBorder, scoreAreaBorder)) {//不在得分区
+                this.outScoreArea = true;
+                if (this.inScoreArea) {
+                    //如果前一状态仍然表示停留在得分区，则表明：
+                    //小鸟刚刚安全通过得分区，应当加分
+                    this.inScoreArea = false;
+                    score.increase();
+                    //播放音效
+                    Music.getInstance().playScore();
+                    break;
+                }
+            } else {   // 当前进入得分区
+                this.inScoreArea = true;
+                this.outScoreArea = false;
+                break;
+            }
         }
     }
 
@@ -136,15 +146,15 @@ export class Director {
         if (!this.isGameOver) { //只有当游戏未结束时才运行游戏场景
             //找到背景精灵对象并执行绘制
             //此处采用默认值绘制
-            this.dataStore.get('background').draw();   
-            
+            this.dataStore.get('background').draw();
+
             //是否产生新的水管组
             const pipes = this.dataStore.get('pipes');
             //水管离开屏幕后清除掉
-            if (pipes[0].x + pipes[0 ].width <= 0 && pipes.length == 4) {
+            if (pipes[0].x + pipes[0].width <= 0 && pipes.length == 4) {
                 //将退出场景的一组水管从数组中移除
                 pipes.shift();
-                pipes.shift();    
+                pipes.shift();
             }
             //前一组水管越过中线，且场景中仅有1组水管
             const cvs = this.dataStore.canvas;
@@ -156,7 +166,7 @@ export class Director {
                 values.draw();
             });
             //绘制地面 
-            this.dataStore.get('land').draw();  
+            this.dataStore.get('land').draw();
 
             //实现动画：循环重绘场景
             let timer = requestAnimationFrame(() => this.run());
